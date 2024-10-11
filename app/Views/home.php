c4dd95446f8971648f769da7a32b4be3f27ea2e8
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2024-10-11 15:05:39 +0700

    home - WIP

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,5 +1,13 @@
 <?= $this->extend('layout') ?>
 
 <?= $this->section('main') ?>
-<div uk-height-viewport></div>
+    <section class="uk-section-muted uk-inverse-dark uk-section uk-section-xlarge uk-padding-remove-bottom" uk-scrollspy="target: [uk-scrollspy-class]; cls: uk-animation-fade; delay: false;">
+        <div class="uk-container uk-container-xlarge">
+            <div class="uk-grid tm-grid-expand uk-child-width-1-1 uk-grid-margin">
+                <div class="uk-width-1-1 uk-text-center">
+                    <img class="uk-width-1-1 uk-width-2-3@m" src="images/logo.png"/>
+                </div>
+            </div>
+        </div>
+    </section>
 <?= $this->endSection() ?>
