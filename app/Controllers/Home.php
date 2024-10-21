c6bc82fd2b16959557cb9f25b7227e4d58652f2b
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2024-10-21 11:17:16 +0700

    profile - WIP

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,4 +18,19 @@ class Home extends BaseController
         // Rendering View
         return view('home', $data);
     }
+
+    public function profile()
+    {
+        // Calling Models
+
+        // Populating Data
+
+        // Parsing Data to View
+        $data                   = $this->data;
+        $data['title']          = 'expect - Training Consultant';
+        $data['description']    = 'Bawa ide aplikasi Anda menjadi kenyataan dengan Kodebiner! Kami membengun aplikasi sesuai dengan kebutuhan bisnis Anda.';
+
+        // Rendering View
+        return view('home', $data);
+    }
 }
