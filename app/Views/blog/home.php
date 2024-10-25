852cef6a124c002ad7f50901e648314255f54fdf
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2024-10-25 11:18:24 +0700

    blog - WIP

--- app/Views/blog/home.php
+++ app/Views/blog/home.php
@@ -11,10 +11,10 @@
             <div class="uk-container uk-container-xlarge">
                 <div class="tm-grid-expand uk-margin-xlarge" uk-grid>
                     <div class="uk-width-3-4@s">
-                        <h1 class="uk-heading-large uk-position-relative uk-margin-medium uk-scrollspy-inview" uk-scrollspy-class>
+                        <h1 class="uk-heading-large uk-position-relative uk-margin-medium" uk-scrollspy-class>
                             Update & Inspirasi
                         </h1>
-                        <div class="uk-panel uk-text-lead uk-position-relative uk-margin-large uk-margin-remove-top uk-width-2xlarge uk-scrollspy-inview" uk-scrollspy-class>
+                        <div class="uk-panel uk-text-lead uk-position-relative uk-margin-large uk-margin-remove-top uk-width-2xlarge" uk-scrollspy-class>
                             Lorem ipsum dolor sit amet, consectur adipiscing elit, usmod tempor incididunt ut labore et dolore magna aliqua enim minim veniam.
                         </div>
                     </div>
@@ -32,12 +32,166 @@
                             <div class="uk-child-width-1-1 uk-child-width-1-2@s" uk-grid>
                                 <div>
                                     <a href="blog/dummyarticle">
-                                        <article class="el-item uk-panel uk-margin-remove-first-child uk-scrollspy-inview" uk-scrollspy-class>
-                                            <div class="picture-continer">
-                                                
+                                        <article class="el-item uk-panel uk-margin-remove-first-child" uk-scrollspy-class>
+                                            <div class="uk-height-large uk-background-cover"  data-src="images/outbound.jpg" uk-img></div>
+                                            <h3 class="el-title uk-h3 uk-link-reset uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
                                             </div>
                                         </article>
                                     </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-panel uk-margin-remove-first-child" uk-scrollspy-class>
+                                            <div class="uk-height-large uk-background-cover"  data-src="images/pelatihan-pra-purnabakti.png" uk-img></div>
+                                            <h3 class="el-title uk-h3 uk-link-reset uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <div class="uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-grid-row-large uk-grid-match" uk-grid>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/outbound.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/pelatihan-pra-purnabakti.png" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/pelatihan-inhouse.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/pelatihan-public.png" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/gallery/dummy-2.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/gallery/dummy-1.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/gallery/dummy-5.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="blog/dummyarticle">
+                                        <article class="el-item uk-grid-item-match" uk-scrollspy-class>
+                                            <picture class="uk-cover-container">
+                                                <canvas width="370" height="208"></canvas>
+                                                <img src="images/gallery/dummy-3.jpg" uk-cover />
+                                            </picture>
+                                            <h3 class="el-title uk-h4 uk-margin-top uk-margin-remove-bottom">Lorem Ipsum Dolor Sit Amet</h3>
+                                            <div class="uk-margin-small-top">
+                                                <div class="el-link uk-button uk-button-text">Selengkapnya</div>
+                                            </div>
+                                        </article>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-grid-margin uk-container uk-container-small">
+                    <div class="uk-grid tm-grid-expand uk-child-width-1-1">
+                        <div class="uk-grid-item-match uk-width-1-1@m">
+                            <div class="uk-tile-secondary uk-tile">
+                                <h2 class="uk-margin-medium uk-text-center" uk-scrollspy-class>
+                                    Berlangganan Newsletter kami
+                                    <br/>
+                                    Dapatkan Update Berkala
+                                </h2>
+                                <div class="uk-width-2xlarge uk-margin-auto@s uk-margin-auto uk-text-center" uk-scrollspy-class>
+                                    <form class="uk-form uk-panel js-form-newsletter">
+                                        <div class="uk-grid-small" uk-grid>
+                                            <div class="uk-width-1-1 uk-width-expand@m">
+                                                <input class="el-input uk-input" type="email" placeholder="Alamat Email" />
+                                            </div>
+                                            <div class="uk-width-1-1 uk-width-auto@m uk-text-center@m">
+                                                <button class="el-button uk-button uk-button-default" type="submit">Berlangganan</button>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
